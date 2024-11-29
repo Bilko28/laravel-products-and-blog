a453ab20acae9b8c3457a45da84e9262642e014f
Ash Coles <ashley@acoles.co.uk>
2024-11-29 10:53:06 +0000

    Displaying original input values on add form

--- resources/views/addProduct.blade.php
+++ resources/views/addProduct.blade.php
@@ -5,7 +5,7 @@
     @csrf
     <div>
         <label for="name">Name</label>
-        <input type="text" id="name" name="name" />
+        <input type="text" id="name" name="name" value="{{ old('name') }}" />
 
         @error('name')
             <p class="text-red-600">{{ $message }}</p>
@@ -14,7 +14,7 @@
 
     <div>
         <label for="description">Description</label>
-        <input type="text" id="description" name="description" />
+        <input type="text" id="description" name="description" value="{{ old('description') }}" />
 
         @error('description')
             <p class="text-red-600">{{ $message }}</p>
@@ -23,7 +23,7 @@
 
     <div>
         <label for="image">Image URL</label>
-        <input type="text" id="image" name="image" />
+        <input type="text" id="image" name="image" value="{{ old('image') }}" />
 
         @error('image')
             <p class="text-red-600">{{ $message }}</p>
@@ -32,7 +32,7 @@
 
     <div>
         <label for="quantity">Quantity</label>
-        <input type="text" id="quantity" name="quantity" />
+        <input type="text" id="quantity" name="quantity" value="{{ old('quantity') }}" />
 
         @error('quantity')
             <p class="text-red-600">{{ $message }}</p>
@@ -41,7 +41,7 @@
 
     <div>
        <label for="price">Price</label>
-        <input type="text" id="price" name="price" />
+        <input type="text" id="price" name="price" value="{{ old('price') }}" />
 
         @error('price')
             <p class="text-red-600">{{ $message }}</p>
